6ae43b31245506e8d808c18a961ec465fbadbaea
taesung1993 <cheonyulin@gmail.com>
2022-11-06 19:23:12 +0900

    feat: theme 프로바이더, 리코일 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { RecoilRoot } from 'recoil';
 import GlobalStyle from './components/GlobalStyle';
 import Router from './components/Router';
+import theme from './utils/constants/theme';
 
 function App() {
   return (
     <div className='App'>
-      <GlobalStyle />
-      <Router />
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Router />
+        </ThemeProvider>
+      </RecoilRoot>
     </div>
   );
 }
