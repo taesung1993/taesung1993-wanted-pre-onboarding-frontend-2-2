f826afd774e351a7326d982b61d742dde9e2daca
taesung1993 <cheonyulin@gmail.com>
2022-11-06 19:25:26 +0900

    feat: 아이콘 컴포넌트 추가

---
+++ src/components/icons/index.js
@@ -0,0 +1,11 @@
+import Logo from './Logo';
+import DashBoard from './DashBoard';
+import ManageAd from './ManageAd';
+import Guide from './Guide';
+
+export default {
+  Logo,
+  DashBoard,
+  ManageAd,
+  Guide
+};
