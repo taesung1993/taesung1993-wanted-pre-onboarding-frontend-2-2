945678cf3d0102a76cbbff74282e17b06eba81f1
taesung1993 <cheonyulin@gmail.com>
2022-12-04 18:37:46 +0900

    feat: add css properties

--- src/components/atoms/Wrapper.js
+++ src/components/atoms/Wrapper.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Wrapper = styled.section`
   width: ${({ width }) => width || 'auto'};
   height: ${({ height }) => height || 'auto'};
+  max-height: ${({ maxHeight }) => maxHeight || 'auto'};
   padding: ${({ padding }) => padding || '0px'};
   margin: ${({ margin }) => margin || '0px'};
   flex: ${({ flex }) => flex || 'none'};
@@ -11,9 +12,16 @@ const Wrapper = styled.section`
   align-items: ${({ alignItems }) => alignItems || 'flex-start'};
   justify-content: ${({ justifyContent }) => justifyContent || 'flex-start'};
   gap: ${({ gap }) => gap || 'auto'};
+  border: ${({ border }) => border};
   border-radius: ${({ borderRadius }) => borderRadius || '0px'};
   background-color: ${({ backgroundColor }) =>
     backgroundColor || 'transparent'};
+  position: ${({ position }) => position};
+  z-index: ${({ zIndex }) => zIndex};
+  top: ${({ top }) => top};
+  left: ${({ left }) => left};
+  overflow-x: ${({ overflowX }) => overflowX};
+  overflow-y: ${({ overflowY }) => overflowY};
 `;
 
 export default Wrapper;
